f37ca0989ff271d292f684644647694328e68195
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-04-25 15:36:11 +0900

    add `DanbiNetworkSelect::Model`

--- danbi_server/DanbiNetworkSelect.h
+++ danbi_server/DanbiNetworkSelect.h
@@ -3,22 +3,17 @@
 #include "Types.h"
 
 #ifdef _WINDLL
-#define NETWORK_DLL_API __declspec(dllexport)
+#define EXPORT_NETWORK_DLL_API __declspec(dllexport)
 #else
-#define NETWORK_DLL_API __declspec(dllimport)
+#define EXPORT_NETWORK_DLL_API __declspec(dllimport)
 #endif
 
 namespace DanbiNetworkSelect
 {
-	enum class eInitMode
-	{
-		INIT_BEFORE,
-		INIT_SUCCESS,
-		INIT_FAILED,
-	};
+	struct Model;
 
-	extern "C" NETWORK_DLL_API
-	eInitMode Initialize(
+	extern "C" EXPORT_NETWORK_DLL_API
+	Model* CreateOrNull(
 		const wchar_t* const lpListenSocketIpV4,
 		const unsigned short listenSocketTcpPort,
 		const unsigned long nonBlockingMode,
@@ -29,13 +24,12 @@ namespace DanbiNetworkSelect
 		const size_t recvRingBufferCapacity,
 		bool (*pOnClientJoined)(const SESSION_ID sessionId),
 		void (*pOnClientLeaved)(const SESSION_ID sessionId),
-		void (*pOnMessageRecieved)(const SESSION_ID sessionId, const uint32_t cbRecv, const char* const pData)
+		void (*pOnMessageRecieved)(const SESSION_ID sessionId, const uint32_t cbRecv, const char* const pMessage)
 	);
 
-	extern "C" NETWORK_DLL_API
-	void Finalize(void);
+	extern "C" EXPORT_NETWORK_DLL_API
+	void Finalize(Model* const pModel);
 
-	extern int gTcpNoDelay;
-	extern size_t gSendRingBufferCapacity;
-	extern size_t gRecvRingBufferCapacity;
+	extern "C" EXPORT_NETWORK_DLL_API
+	void Update(Model* const pModel);
 }
--- danbi_server/DanbiServer.cpp
+++ danbi_server/DanbiServer.cpp
@@ -3,30 +3,35 @@
 
 #pragma comment(lib,"./DanbiNetworkSelect.lib")
 
-int RunServer(void);
+int RunServer(DanbiNetworkSelect::Model* const pModel);
 bool OnClientJoined(const SESSION_ID sessionId);
 void OnClientLeaved(const SESSION_ID sessionId);
 void OnMessageRecieved(const SESSION_ID sessionId, const uint32_t cbRecv, const char* const pData);
 
 int wmain()
 {
-	DanbiNetworkSelect::eInitMode initMode;
+	DanbiNetworkSelect::Model* pModelOrNull;
 
-	initMode = DanbiNetworkSelect::Initialize(L"0.0.0.0", 9000, 1, 65535, 1, true, OnClientJoined, OnClientLeaved, OnMessageRecieved);
-	if (initMode != DanbiNetworkSelect::eInitMode::INIT_SUCCESS)
+	pModelOrNull = DanbiNetworkSelect::CreateOrNull(L"0.0.0.0", 9000, 1, 65535, 1, true, 2048, 1024, OnClientJoined, OnClientLeaved, OnMessageRecieved);
+	if (pModelOrNull == nullptr)
 	{
 		return 1;
 	}
 
-	RunServer();
+	RunServer(pModelOrNull);
 
-	DanbiNetworkSelect::Finalize();
+	DanbiNetworkSelect::Finalize(pModelOrNull);
 
 	return 0;
 }
 
-int RunServer(void)
+int RunServer(DanbiNetworkSelect::Model* const pModel)
 {
+	for (;;)
+	{
+		DanbiNetworkSelect::Update(pModel);
+	}
+
 	return 0;
 }
 
