a88f21491446ab05d2b776eda79656a88fcb245b
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-04-25 16:52:46 +0900

    Update Main.cpp

--- danbi_server/Main.cpp
+++ danbi_server/Main.cpp
@@ -6,7 +6,15 @@ int wmain()
 {
 	DanbiNetworkSelect::Instance* pInstanceOrNull;
 
-	pInstanceOrNull = DanbiNetworkSelect::CreateOrNull(L"0.0.0.0", 9000, 1, 65535, 1, true, 2048, 1024, DanbiServer::OnClientJoined, DanbiServer::OnClientLeaved, DanbiServer::OnMessageRecieved);
+	pInstanceOrNull = DanbiNetworkSelect::CreateOrNull(
+		L"0.0.0.0", 9000,
+		1, 65535, 1, true,
+		2048, 1024,
+		DanbiServer::OnClientJoined,
+		DanbiServer::OnClientLeaved,
+		DanbiServer::OnMessageRecieved
+	);
+
 	if (pInstanceOrNull != nullptr)
 	{
 		DanbiServer::Run(pInstanceOrNull);
