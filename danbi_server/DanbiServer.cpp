90121c30c3051500cf296c7cbfcb3d8a1d2198f1
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-05-08 00:30:04 +0900

    update

--- danbi_server/DanbiServer.cpp
+++ danbi_server/DanbiServer.cpp
@@ -1,4 +1,7 @@
+#include <cstdio>
+
 #include "DanbiServer.h"
+#include "SerializedBuffer.h"
 
 int DanbiServer::Run(DanbiNetworkSelect::Instance* const pInstance)
 {
@@ -22,5 +25,6 @@ void DanbiServer::OnClientLeaved(const SESSION_ID sessionId)
 
 bool DanbiServer::OnMessageReceived(const SESSION_ID sessionId, const MESSAGE_TYPE type, SerializedBuffer& msg)
 {
+	wprintf(L"sessionID = %llu\ttype = %hu\tmsg.size=%zu\n", sessionId, type, msg.Size());
 	return true;
 }
