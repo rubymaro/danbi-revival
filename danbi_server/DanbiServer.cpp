e7151f731a633c90915951cbd3b8089aef90bdaf
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-05-08 21:29:54 +0900

    Update DanbiServer.cpp

--- danbi_server/DanbiServer.cpp
+++ danbi_server/DanbiServer.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <Windows.h>
 
 #include "DanbiServer.h"
 #include "SerializedBuffer.h"
@@ -15,16 +16,18 @@ int DanbiServer::Run(DanbiNetworkSelect::Instance* const pInstance)
 
 bool DanbiServer::OnClientJoined(const SESSION_ID sessionId)
 {
+	wprintf(L"[%s:%5u] sessionId = %llu\n", TEXT(__FUNCTION__), GetCurrentThreadId(), sessionId);
+
 	return true;
 }
 
 void DanbiServer::OnClientLeaved(const SESSION_ID sessionId)
 {
-
+	wprintf(L"[%s:%5u] sessionId = %llu\n", TEXT(__FUNCTION__), GetCurrentThreadId(), sessionId);
 }
 
 bool DanbiServer::OnMessageReceived(const SESSION_ID sessionId, const MESSAGE_TYPE type, SerializedBuffer& msg)
 {
-	wprintf(L"sessionID = %llu\ttype = %hu\tmsg.size=%zu\n", sessionId, type, msg.Size());
+	wprintf(L"[%s:%5u] sessionId = %llu\ttype = %hu\tmsg.size=%zu\n", TEXT(__FUNCTION__), GetCurrentThreadId(), sessionId, type, msg.Size());
 	return true;
 }
