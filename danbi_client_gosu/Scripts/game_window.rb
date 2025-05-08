29eef897540a50e339f408a64672c312c16c3cd8
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-05-08 23:35:58 +0900

    add danbi_client_gosu

---
+++ danbi_client_gosu/Scripts/game_window.rb
@@ -0,0 +1,15 @@
+require "gosu"
+
+class GameWindow < Gosu::Window
+    def initialize
+      super(800, 600)
+      self.caption = "Danbi Revival"
+    end
+    
+    def update
+    end
+    
+    def draw
+    end
+  end
+  
---
+++ danbi_client_gosu/Scripts/main.rb
@@ -0,0 +1,4 @@
+require_relative "./game_window.rb"
+
+$game_window = GameWindow.new()
+$game_window.show
