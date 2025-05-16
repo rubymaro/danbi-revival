0a2da070a31f23a55b109ad56fcd0a8fe5571dcf
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-05-16 17:51:05 +0900

    Update Main.rb

--- danbi_client_gosu/Scripts/Main.rb
+++ danbi_client_gosu/Scripts/Main.rb
@@ -2,12 +2,16 @@ if __FILE__ == $0
   $LOAD_PATH.unshift(File.expand_path("../", __dir__))
 
   require "gosu"
+  require "chunky_png"
+
+  require_relative "Extensions/Gosu_ImageEx.rb"
 
   require_relative "Config.rb"
   require_relative "GameWindow.rb"
 
   require_relative "MUI3/MUI3_Manager.rb"
   require_relative "MUI3/Components/MUI3_Component.rb"
+  require_relative "MUI3/MUI3_Style.rb"
   require_relative "MUI3/Components/MUI3_Form.rb"
 
   require_relative "MUI3/Outputs/MUI3_Form_Login.rb"
