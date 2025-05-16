a9b00c7568d65ef2efbf044bf7338bf1cdacd85e
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-05-16 17:51:03 +0900

    Update MUI3_Form.rb

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_Form.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_Form.rb
@@ -1,6 +1,7 @@
 class MUI3::Form < MUI3::Component
   def initialize(x:, y:, width:, height:)
     super(x: x, y: y, width: width, height: height)
+    @skin = MUI3::Style::WhiteForm.create(width: width, height: height)
   end
 
   def update
@@ -8,6 +9,6 @@ class MUI3::Form < MUI3::Component
   end
 
   def draw
-    
+    @skin.draw(x: @x, y: @y)
   end
 end
