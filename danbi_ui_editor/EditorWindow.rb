acc7efc2db8a0ce03e8ba10991c4e8e9a0a86376
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-08 00:49:08 +0900

    update

--- danbi_ui_editor/EditorWindow.rb
+++ danbi_ui_editor/EditorWindow.rb
@@ -2,7 +2,12 @@ script_path = File.expand_path("../", __dir__) + "/danbi_client_gosu/Scripts/"
 $LOAD_PATH.unshift(script_path)
 
 require "gosu"
+require "chunky_png"
+
+require "Extensions/Gosu_ImageEx.rb"
 require "MUI3/requires.rb"
+
+require_relative "Forms/Test.rb"
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -10,10 +15,18 @@ class DanbiUIEditorWindow < Gosu::Window
   def initialize
     super(WIDTH, HEIGHT)
     self.caption = "Danbi UI Editor"
+    $mui_manager = MUI3::Manager.new(gosu_window: self)
+    @form_test = MUI3::Form::Test.new
+    $mui_manager.add(component: @form_test)
+  end
+
+  def update
+    $mui_manager.update
   end
 
   def draw
+    $mui_manager.draw
   end
 end
 
-DanbiUIEditorWindow.new.show if __FILE__ == $0
+DanbiUIEditorWindow.new.show
---
+++ danbi_ui_editor/Forms/Test.rb
@@ -0,0 +1,8 @@
+class MUI3::Form::Test < MUI3::Form
+  def initialize
+    super(x: 200, y: 200, width: 400, height: 300)
+
+    @text = MUI3::Text.new(x: 20, y: 20, width: @width, font_size: 20, text: "테스트")
+    add_child(component: @text)
+  end
+end
