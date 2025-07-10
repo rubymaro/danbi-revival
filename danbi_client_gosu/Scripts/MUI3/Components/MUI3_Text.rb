b93093e3928e65d3437e9bbbcc37fbc8dc059258
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-10 14:48:03 +0900

    Center text in MUI3::Button and add alignment to MUI3::Text
    Updated MUI3::Button to center its text vertically and horizontally by passing width, height, and align parameters to MUI3::Text and adjusting the y position. Enhanced MUI3::Text to support text alignment and to expose the font via an attr_reader.

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_Text.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_Text.rb
@@ -1,12 +1,16 @@
 class MUI3::Text < MUI3::Component
-  def initialize(x:, y:, width: nil, height: nil, text: "MUI3::Text#{self.object_id}", font_name: "Malgun Gothic", font_height: 20, font_color: Gosu::Color::BLACK)
+  attr_reader(:font)
+
+  def initialize(x:, y:, width: nil, height: nil, text: "MUI3::Text#{self.object_id}",
+    font_name: "Malgun Gothic", font_height: 20, font_color: Gosu::Color::BLACK, align: :left)
     @font = Gosu::Font.new(font_height, {:name => font_name})
     width = @font.text_width(text) if width.nil?
     height = font_height if height.nil?
-    super(x: x, y: y, width: width, height: height)
+    @align = align
     @text = text
     @font_color = font_color
-    @gosu_image_text = Gosu::Image.from_text(@text, @font.height, {:width => @width, :font => @font.name})
+    @gosu_image_text = Gosu::Image.from_text(@text, @font.height, {:width => width, :font => @font.name, :align => @align})
+    super(x: x, y: y, width: width, height: @gosu_image_text.height)
   end
 
   def update
