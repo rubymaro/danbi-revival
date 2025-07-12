0aaff77cd144067caeb188537cde9c873d08acb9
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-12 20:04:20 +0900

    Update MUI3_Button.rb

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_Button.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_Button.rb
@@ -12,8 +12,14 @@ class MUI3::Button < MUI3::Component
     @text_caption = MUI3::Text.new(x: 0, y: 0, text: text, width: width, align: :center, font_color: Gosu::Color::WHITE)
     @text_caption.y += (@height - @text_caption.height) / 2
     add_child(component: @text_caption)
-    register_event_handler(type: :mouse_over, proc: proc { @image_buttonset.subimage!(x: 0, y: @height * MOUSE_OVER, width: @width, height: @height) })
-    register_event_handler(type: :mouse_out, proc: proc { @image_buttonset.subimage!(x: 0, y: @height * DEFAULT, width: @width, height: @height) })
+    proc_mouse_over = proc {
+      @image_buttonset.subimage!(x: 0, y: @height * MOUSE_OVER, width: @width, height: @height)
+    }
+    proc_mouse_out = proc {
+      @image_buttonset.subimage!(x: 0, y: @height * DEFAULT, width: @width, height: @height)
+    }
+    register_event_handler(type: :mouse_over, proc: proc_mouse_over)
+    register_event_handler(type: :mouse_out, proc: proc_mouse_out)
   end
 
   def update
