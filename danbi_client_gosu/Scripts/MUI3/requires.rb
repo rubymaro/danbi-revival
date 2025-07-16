9b9d37ea512add66041025795a0fdc75de2437e7
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-16 11:33:04 +0900

    Move MUI3_Image to Components and update require path
    Renamed MUI3_Image.rb to Components/MUI3_Image.rb and updated the require path in requires.rb to reflect the new location for better organization.

--- danbi_client_gosu/Scripts/MUI3/requires.rb
+++ danbi_client_gosu/Scripts/MUI3/requires.rb
@@ -3,6 +3,6 @@ require_relative "Components/MUI3_Component.rb"
 require_relative "Components/MUI3_Form.rb"
 require_relative "Components/MUI3_Text.rb"
 require_relative "Components/MUI3_Button.rb"
-require_relative "MUI3_Image.rb"
+require_relative "Components/MUI3_Image.rb"
 
 require_relative "Outputs/MUI3_Form_Login.rb"
