369d1af8ba061972f296be655c8ead918d533a67
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-16 15:39:24 +0900

    Update MUI3_Form.rb

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_Form.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_Form.rb
@@ -1,7 +1,6 @@
 class MUI3::Form < MUI3::Component
   def initialize(x:, y:, width:, height:, image_style: MUI3::Image::WhiteForm)
     super(x: x, y: y, width: width, height: height)
-    @dragged = false
     @image_bg = image_style.create(x: 0, y: 0, width: width, height: height)
     add_child(component: @image_bg)
     proc_mouse_drag = proc { |component, dx:, dy:|
