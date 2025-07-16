52ca73a3399efcdc7ef536d9618eb1aa2b740df4
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-16 15:29:58 +0900

    Refactor mouse drag event handler in MUI3_Form
    Extracted the mouse drag event handler into a separate variable for improved readability and maintainability.

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_Form.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_Form.rb
@@ -4,10 +4,11 @@ class MUI3::Form < MUI3::Component
     @dragged = false
     @image_bg = image_style.create(x: 0, y: 0, width: width, height: height)
     add_child(component: @image_bg)
-    register_event_handler(type: :mouse_drag, proc: proc { |component, dx:, dy:|
+    proc_mouse_drag = proc { |component, dx:, dy:|
       @x += dx
       @y += dy
-    })
+    }
+    register_event_handler(type: :mouse_drag, proc: proc_mouse_drag)
   end
 
   def update
