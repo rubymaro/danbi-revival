5d9d9d65560bba8d5e8c79526fccc0243fed2d38
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-23 18:06:41 +0900

    Add input box state visuals for focus handling
    Introduces constants for input box states and updates the background image to reflect focus changes. The input box now visually indicates when it is focused or unfocused by changing its subimage.

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_InputBox.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_InputBox.rb
@@ -1,19 +1,25 @@
 class MUI3::InputBox < MUI3::Component
+  DEFAULT = 0
+  FOCUSED = 1
+  DISABLED = 2
+
   attr_reader(:gosu_text_input)
 
   def initialize(x:, y:, width:, height:, text: "", image_style: MUI3::Image::InputBoxSet)
     super(x: x, y: y, width: width, height: height)
     @text = text
     @gosu_text_input = Gosu::TextInput.new
-    @image_bg = image_style.create(x: 0, y: 0, width: width, height: height)
-    add_child(component: @image_bg)
+    @image_input_box_set = image_style.create(x: 0, y: 0, width: width, height: height)
+    add_child(component: @image_input_box_set)
     @text = MUI3::Text.new(x: 0, y: 0, text: text, width: width, align: :left, font_color: Gosu::Color::BLACK)
     add_child(component: @text)
     proc_got_focus = proc {
       $mui_manager.set_input_box(input_box: self)
+      @image_input_box_set.subimage!(ox: 0, oy: @height * FOCUSED, width: @width, height: @height)
     }
     proc_lost_focus = proc {
       $mui_manager.set_input_box(input_box: nil)
+      @image_input_box_set.subimage!(ox: 0, oy: @height * DEFAULT, width: @width, height: @height)
     }
     register_event_handler(type: :got_focus, proc: proc_got_focus)
     register_event_handler(type: :lost_focus, proc: proc_lost_focus)
