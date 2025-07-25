bbc81f8010cf0e5c421a84ffa0aa20425d0c70ed
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-07-25 20:52:50 +0900

    Set input box text alignment to left
    Changed the text alignment in MUI3_InputBox from right to left to improve text input usability and match typical input box behavior.

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_InputBox.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_InputBox.rb
@@ -7,11 +7,10 @@ class MUI3::InputBox < MUI3::Component
 
   def initialize(x:, y:, width:, height:, text: "", image_style: MUI3::Image::InputBoxSet)
     super(x: x, y: y, width: width, height: height)
-    @text = text
     @gosu_text_input = Gosu::TextInput.new
     @image_input_box_set = image_style.create(x: 0, y: 0, width: width, height: height)
     add_child(component: @image_input_box_set)
-    @text = MUI3::Text.new(x: 4, y: 4, text: text, width: width - 8, align: :right, font_color: Gosu::Color::BLACK)
+    @text = MUI3::Text.new(x: 4, y: 4, text: text, width: width - 8, align: :left, font_color: Gosu::Color::BLACK)
     add_child(component: @text)
     proc_got_focus = proc {
       $mui_manager.set_input_box(input_box: self)
