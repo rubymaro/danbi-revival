d87f0f260e205248e4931d65cc76d17ab7ee7fae
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2025-08-04 17:46:27 +0900

    Refactor MUI3_Text to use Gosu::Font object
    Replaces direct font parameters with a Gosu::Font instance for improved font handling. Updates width calculation and image text generation to use the new font object, simplifying initialization and rendering logic.

--- danbi_client_gosu/Scripts/MUI3/Components/MUI3_Text.rb
+++ danbi_client_gosu/Scripts/MUI3/Components/MUI3_Text.rb
@@ -3,20 +3,20 @@ class MUI3::Text < MUI3::Component
 
   def initialize(x:, y:, width: nil, text: "MUI3::Text#{self.object_id}",
     font_name: Config::FONT_NAME, font_height: Config::FONT_HEIGHT, font_color: Gosu::Color::BLACK, align: :left)
-    @gosu_image_text = Gosu::Image.from_text(text, font_height, {:width => width, :font => font_name, :align => align})
+    @gosu_font = Gosu::Font.new(font_height, {:name => font_name})
+    width ||= @gosu_Font.text_width(text)
     @align = align
     @text = text
-    @font_name = font_name
     @font_color = font_color
-    @font_height = font_height
-    super(x: x, y: y, width: @gosu_image_text.width, height: @gosu_image_text.height)
+    super(x: x, y: y, width: width, height: font_height)
+    update_image_text
   end
 
   def update
   end
 
   def update_image_text
-    @gosu_image_text = Gosu::Image.from_text(@text, @font_height, {:width => @width, :font => @font_name, :align => @align})
+    @gosu_image_text = Gosu::Image.from_text(@text, @gosu_font.height, {:width => @width, :font => @gosu_font.name, :align => @align})
   end
 
   def draw
